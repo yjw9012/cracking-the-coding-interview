e19d46b11205384675e9f470789fe1d20e0517cc
Jiwoong Youn <jiwoong.youn@appdynamics.com>
2017-07-12 20:44:13 -0700

    Initial commit + problem 16.6

---
+++ chapter-16/16.6-smallest_difference.py
@@ -0,0 +1,34 @@
+def get_smallest_diff(arr1, arr2):
+    smallest_diff = 10000000
+    for i in range(len(arr1)):
+        for j in range(len(arr2)):
+            diff = abs(arr1[i] - arr2[j])
+            if diff < smallest_diff:
+                smallest_diff = diff
+
+    return smallest_diff
+
+
+def get_smallest_diff_better(arr1, arr2):
+    smallest_diff = 10000000
+    arr1.sort()
+    arr2.sort()
+    arr1_idx = 0
+    arr2_idx = 0
+    while arr1_idx < len(arr1) and arr2_idx < len(arr2):
+        diff = abs(arr1[arr1_idx] - arr2[arr2_idx])
+        if diff == 0:
+            return 0
+        if diff < smallest_diff:
+            smallest_diff = diff
+        if arr1[arr1_idx] < arr2[arr2_idx]:
+            arr1_idx += 1
+        else:
+            arr2_idx += 1
+    return smallest_diff
+
+
+arr1 = [1, 3, 15, 11, 2]
+arr2 = [23, 127, 235, 19, 8]
+
+print(get_smallest_diff_better(arr1, arr2))
