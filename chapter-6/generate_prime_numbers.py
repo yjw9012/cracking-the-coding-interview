9ce98154a1b24de404d3d9fd25d1a7f55ddf75e4
Jiwoong Youn <yjw9012job@gmail.com>
2017-08-19 11:32:49 -0700

    Fix variable name

--- chapter-6/generate_prime_numbers.py
+++ chapter-6/generate_prime_numbers.py
@@ -20,6 +20,6 @@ def find_next_prime(flags, num, n):
 if __name__ == '__main__':
 
     prime_numbers = generate_prime_numbers(100)
-    prime_numbers_up_to_100 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-    for i in range(len(prime_numbers_up_to_100)):
-        assert prime_numbers[i] == prime_numbers_up_to_100[i]
+    prime_numbers_up_to_hundred = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    for i in range(len(prime_numbers_up_to_hundred)):
+        assert prime_numbers[i] == prime_numbers_up_to_hundred[i]
